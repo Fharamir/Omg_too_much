56c331010fb760da9ca8133881f94237d84f6034
Fharamir <f.ruggieri89@gmail.com>
2017-02-27 09:14:52 +0100

    Base
    Test

---
+++ omgtoomuch/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace omgtoomuch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TT = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // bar
+            // 
+            this.bar.Location = new System.Drawing.Point(8, 9);
+            this.bar.Name = "bar";
+            this.bar.Size = new System.Drawing.Size(655, 18);
+            this.bar.TabIndex = 0;
+            this.bar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.bar_MouseDown);
+            this.bar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.bar_MouseMove);
+            this.bar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.bar_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(662, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 9);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.Close_this);
+            // 
+            // TT
+            // 
+            this.TT.Tick += new System.EventHandler(this.Check);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 33);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Omg Such Load!";
+            this.Load += new System.EventHandler(this.Transparency);
+            this.DoubleClick += new System.EventHandler(this.Close_this);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar bar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer TT;
+    }
+}
+
---
+++ omgtoomuch/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.Devices;
+
+namespace omgtoomuch
+{
+    public partial class Form1 : Form
+    {
+        ulong TotalRam;
+
+        private bool mouseDown;
+        private Point lastLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_this(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Transparency(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            BackColor = Color.Lime;
+            TransparencyKey = Color.Lime;
+
+            this.Location = new Point(0, Screen.PrimaryScreen.WorkingArea.Height - 50);
+            this.Width = Screen.PrimaryScreen.WorkingArea.Width - 10;
+            bar.Width = this.Width - 12;
+            label1.Location = new Point(this.Width - 8, label1.Location.Y);
+            TotalRam = new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory / (ulong)(1024 * 1024);
+            bar.Maximum = 1000;
+            bar.Minimum = 0;
+            bar.Value = 0;
+
+            TT.Interval = 30;
+            TT.Start();
+        }
+
+        private void bar_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+            TT.Stop();
+        }
+
+        private void bar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void bar_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+            TT.Start();
+        }
+
+        private void Check(object sender, EventArgs e)
+        {
+            int val = 1000 - ((int)(Math.Round((double)(getAvailableRAM() * 100 / TotalRam),1)) * 10);
+            if (val < 0) val = 0;
+            if (val > 1000) val = 1000;
+
+            if (val < 500)
+            { ModifyProgressBarColor.SetState(bar, 1); }
+            else
+                if (val < 750)
+                { ModifyProgressBarColor.SetState(bar, 3); }
+                else
+                { ModifyProgressBarColor.SetState(bar, 2); }
+
+            bar.Value = val;
+        }
+
+        public ulong getAvailableRAM()
+        {
+            return new Microsoft.VisualBasic.Devices.ComputerInfo().AvailablePhysicalMemory / (ulong)(1024 * 1024);
+        }
+    }
+}
